24f46217509bb37302bfef8412e43b3be4dc08cd
haoxin <coderhaoxin@outlook.com>
2015-07-28 11:08:18 +0800

    v0.1.0: init

---
+++ Readme.md
@@ -0,0 +1,29 @@
+
+[![NPM version][npm-img]][npm-url]
+[![Build status][travis-img]][travis-url]
+[![Test coverage][coveralls-img]][coveralls-url]
+[![License][license-img]][license-url]
+[![Dependency status][david-img]][david-url]
+
+### koao-knex
+
+* [knex](https://github.com/tgriesser/knex) adaptor for [koao](https://github.com/coderhaoxin/koao)
+
+### Usage
+
+```js
+```
+
+### License
+MIT
+
+[npm-img]: https://img.shields.io/npm/v/koao-knex.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/koao-knex
+[travis-img]: https://img.shields.io/travis/coderhaoxin/koao-knex.svg?style=flat-square
+[travis-url]: https://travis-ci.org/coderhaoxin/koao-knex
+[coveralls-img]: https://img.shields.io/coveralls/coderhaoxin/koao-knex.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/coderhaoxin/koao-knex?branch=master
+[license-img]: https://img.shields.io/badge/license-MIT-green.svg?style=flat-square
+[license-url]: http://opensource.org/licenses/MIT
+[david-img]: https://img.shields.io/david/coderhaoxin/koao-knex.svg?style=flat-square
+[david-url]: https://david-dm.org/coderhaoxin/koao-knex
---
+++ test/index.js
@@ -0,0 +1,92 @@
+'use strict'
+
+const assert = require('assert')
+const Adaptor = require('../')
+
+describe('## koao-knex', function() {
+  describe('# basic', function() {
+    let table = 'testkoao'
+    let adaptor
+    let id
+
+    it('init', function() {
+      return new Promise(function(resolve, reject) {
+        adaptor = new Adaptor({
+          client: 'pg',
+          connection: 'postgres://koaoknex@localhost/koaoknex',
+          pool: {
+            min: 5,
+            max: 10
+          },
+
+          table: table,
+          createTable: function(t) {
+            t.increments('id').primary()
+            t.string('name', 100)
+            t.string('desc', 100)
+          },
+
+          connected: resolve,
+          onerror: reject
+        })
+      })
+    })
+
+    it('create', function() {
+      return adaptor
+        .create(table, {
+          name: 'haoxin',
+          desc: 'hello'
+        })
+        .then(function(result) {
+          assert.equal(result.inserted, true)
+          id = result.id
+        })
+    })
+
+    it('update', function() {
+      return adaptor
+        .update(table, id, {
+          desc: 'world'
+        })
+        .then(function(count) {
+          assert.equal(count, 1)
+        })
+    })
+
+    it('findOne', function() {
+      return adaptor
+        .findOne(table, id)
+        .then(function(result) {
+          assert.equal(result.name, 'haoxin')
+          assert.equal(result.desc, 'world')
+        })
+    })
+
+    it('query', function() {
+      return adaptor
+        .query(table, {
+          desc: 'world'
+        })
+        .then(function(result) {
+          assert(result.length > 0)
+        })
+    })
+
+    it('remove', function() {
+      return adaptor
+        .remove(table, id)
+        .then(function(count) {
+          assert.equal(count, 1)
+        })
+    })
+
+    it('findOne', function() {
+      return adaptor
+        .findOne(table, id)
+        .then(function(result) {
+          assert.equal(result, undefined)
+        })
+    })
+  })
+})
